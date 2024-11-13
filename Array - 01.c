061a76d75b81599547aef7daf5d871581a32110c
Shahriar Faysal <160359045+shahriarfaysal@users.noreply.github.com>
2024-11-14 00:03:11 +0600

    Create Array - 01.c

---
+++ Array - 01.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+int main()
+{
+    int ar[5] = {10, 20, 30, 40, 50};
+    for(int i = 0; i < 5; i++)
+    {
+        printf("%d\n", ar[i]);
+    }
+    return 0;
+}
